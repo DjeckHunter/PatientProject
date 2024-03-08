95372bddbe1318683ddf93a0ae64578daf6ba1f0
yaka <yaka@epolsoft.com>
2024-03-08 22:55:48 +0300

    migration - update primary key

--- PatientProject.Infrastructure/Migrations/20240308195521_update-primary-key.Designer.cs
+++ PatientProject.Infrastructure/Migrations/20240308195521_update-primary-key.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PatientProject.Infrastructure.Repository;
@@ -11,9 +12,10 @@ using PatientProject.Infrastructure.Repository;
 namespace PatientProject.Infrastructure.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20240308195521_update-primary-key")]
+    partial class updateprimarykey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PatientProject.Infrastructure/Migrations/20240308195521_update-primary-key.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PatientProject.Infrastructure.Migrations
+{
+    public partial class updateprimarykey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Patients_Genders_GenderId",
+                table: "Patients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_GenderId",
+                table: "Patients");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Patients",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "GenderId1",
+                table: "Patients",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Genders",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_GenderId1",
+                table: "Patients",
+                column: "GenderId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Patients_Genders_GenderId1",
+                table: "Patients",
+                column: "GenderId1",
+                principalTable: "Genders",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Patients_Genders_GenderId1",
+                table: "Patients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_GenderId1",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "GenderId1",
+                table: "Patients");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Patients",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Genders",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_GenderId",
+                table: "Patients",
+                column: "GenderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Patients_Genders_GenderId",
+                table: "Patients",
+                column: "GenderId",
+                principalTable: "Genders",
+                principalColumn: "Id");
+        }
+    }
+}
